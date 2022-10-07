be5afff5cde9e7dde71b7af42ffed0e2c91fb40b
Sasireka S <80803086+Sasireka3019@users.noreply.github.com>
2022-10-07 14:44:44 +0530

    Create LC1011-CapacityToShipPackagesWithinDDays.cpp

---
+++ Arrays/LC1011-CapacityToShipPackagesWithinDDays.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    static int binarySearch(int low, int high, vector<int> weights, int days, int sol)
+    {
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2, i = 0, sum = 0, n = weights.size(), flag = 0;
+            for(int w : weights)
+            {
+                sum += w;
+                if(sum > mid)
+                {
+                    sum = w;
+                    i++;
+                }
+            }
+            i++;
+            if(i <= days)
+            {
+                sol = mid;
+            }
+            if(i <= days)
+            {
+                return binarySearch(low, mid-1, weights, days, sol);
+            }
+            else
+            {
+                return binarySearch(mid+1, high, weights, days, sol);
+            }
+        }
+        return sol;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int high = accumulate(weights.begin(), weights.end(), 0), low = *max_element(weights.begin(), weights.end()), sol = 0;       
+        return binarySearch(low, high, weights, days, sol);
+    }
+};
