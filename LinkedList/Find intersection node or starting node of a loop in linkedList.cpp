2e8028da529da43271ff875b587769308e702a33
Sasireka S <80803086+Sasireka3019@users.noreply.github.com>
2022-10-20 19:53:06 +0530

    Create Find intersection node or starting node of a loop in linkedList.cpp

---
+++ LinkedList/Find intersection node or starting node of a loop in linkedList.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    int removeLoop(Node* head)
+    {
+        Node *slow = head, *fast = head;
+        if(head == NULL || head->next == NULL)
+            return;
+        while(slow && fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+                break;
+        }
+        if(slow != fast)
+            return;
+        slow = head;
+        while(slow != fast)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow->data;
+    }
+};Find intersection node or starting node of a loop in linkedList
