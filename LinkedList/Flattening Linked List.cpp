230041a386af678190b2dce37b69e54b11b55c2c
Sasireka S <80803086+Sasireka3019@users.noreply.github.com>
2022-11-16 14:47:08 +0530

    Create Flattening Linked List.cpp

---
+++ LinkedList/Flattening Linked List.cpp
@@ -0,0 +1,142 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+using namespace std;
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Node structure  used in the program
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+*/
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+struct comparator{
+    bool operator()(Node* a, Node* b)
+    {
+        return a->data > b->data;
+    }
+};
+Node *flatten(Node *root)
+{
+   priority_queue<Node *, vector<Node*>, comparator> p;
+   Node *ans = new Node(1);
+   Node *temp = ans;
+   while(root != NULL)
+   {
+       p.push(root);
+       root = root->next;
+   }
+   while(!p.empty())
+   {
+       Node *newNode = new Node(p.top()->data);
+       newNode->next = NULL;
+       ans->bottom = newNode;
+       ans = ans->bottom;
+       if(p.top()->bottom != NULL)
+        p.push(p.top()->bottom);
+       p.pop();
+   }
+   return temp->bottom;
+}
+
