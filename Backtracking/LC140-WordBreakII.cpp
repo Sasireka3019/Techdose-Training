dfbdd7eb8943ed049c1b5c1c576fcccb9e151a60
Sasireka S <80803086+Sasireka3019@users.noreply.github.com>
2022-12-03 16:56:56 +0530

    Create LC140-WordBreakII.cpp

---
+++ Backtracking/LC140-WordBreakII.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> set;
+        vector<string> res;
+        for(auto w: wordDict)
+            set.insert(w);
+        solve(0, s, "", set, res);
+        return res;
+    }
+    void solve(int index, string s, string curr, unordered_set<string> set, vector<string> &res)
+    {
+        int n = s.length();
+        if(index == n)
+        {
+            curr.pop_back();
+            res.push_back(curr);
+        }
+        string str = "";
+        for(int i=index; i<n; i++)
+        {
+            str.push_back(s[i]);
+            if(set.count(str))
+            {
+                solve(i+1, s, curr+str+" ", set, res);
+            }
+        }
+    }
+};
